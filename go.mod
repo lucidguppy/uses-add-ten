3bca8193d3fcd66f602891310e1a6c468a28ab7e
Matthew Karas <1146938+lucidguppy@users.noreply.github.com>
2022-05-21 16:58:50 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lucidguppy/uses-add-ten
+
+go 1.18
+
+require github.com/lucidguppy/add-ten v0.1.0 // indirect
---
+++ uses_add_ten.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	add_ten "github.com/lucidguppy/add-ten"
+)
+
+func main() {
+	x := add_ten.Add_ten(32)
+	fmt.Println(x)
+}
